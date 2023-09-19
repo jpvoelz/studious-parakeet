1534ef8a76690c0fe6d170d023b0610ff32ee689
jpvoelz <101129349+jpvoelz@users.noreply.github.com>
2023-09-19 10:08:32 -0700

    Implement Dijkstra algorithm

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <unordered_map>
 #include <random>
 #include <vector>
+#include <queue>
+#include <limits>
 
 using namespace std;
 
 class Graph {
 
     private:
-
+        int n;
         vector<vector<pair<int, int> > > adj_list;
 
     public:
@@ -29,9 +30,48 @@ class Graph {
 
         }
 
+        vector<int> dijkstra(int s){
+
+            // Create vector for distances and priority queue
+            vector<int> distance(n, numeric_limits<int>::max());
+            priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+
+            // Add the source vertex to the priority queue and set its distance to 0
+            pq.push(make_pair(0, s));
+            distance[s] = 0;
+
+            while(!pq.empty()){
+
+                // Get the vertex at the top of the queue and its distance
+                // Then remove it from the priority queue
+                int u = pq.top().second;
+                int dist_u = pq.top().first;
+                pq.pop();
+
+                // Now iterate through every neighbor of u
+                for (const pair<int,int>& edge: adj_list[u]){
+                    int v = edge.first;
+                    int edge_weight = edge.second;
+                    int new_distance = edge_weight + distance[u];
+                    // Check for relaxation
+                    if(new_distance < distance[v]){
+                        distance[v] = new_distance;
+                        pq.push(make_pair(distance[v], v));
+                    }
+                }
+
+            }
+
+            // Print shortest distances
+            printf("Vertex Distance from Source\n");
+            for (int i = 0; i < n; ++i)
+                printf("%d \t\t %d\n", i, distance[i]);
+        }
+
         Graph(int n, double density, int dist_range){
 
             // Initialize adjacency list
+            this->n = n;
             vector<vector<pair<int, int> > > graph(n);
             adj_list = graph;
 
@@ -62,6 +102,7 @@ int main(){
 
     Graph graph = Graph(10, 0.2, 5);
     graph.print_graph();
+    graph.dijkstra(0);
     return 0;
 
 }
