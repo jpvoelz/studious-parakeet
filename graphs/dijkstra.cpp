56d4178d467b283f43a70389a7d83428d9563cc5
jpvoelz <101129349+jpvoelz@users.noreply.github.com>
2023-09-19 09:41:53 -0700

    Change adjacency list structure and make weights integers

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <random>
+#include <vector>
 
 using namespace std;
 
@@ -8,50 +9,48 @@ class Graph {
 
     private:
 
-        unordered_map<int, unordered_map<int, double> > adj_list;
+        vector<vector<pair<int, int> > > adj_list;
 
     public:
 
-        bool add_edge(int u, int v, double dist){
-            if (adj_list.count(u) == 0 || adj_list.count(v) == 0)
-                return false;
-            adj_list[u][v] = dist;
-            adj_list[v][u] = dist;
-            return true;
+        void add_edge(int u, int v, int dist){
+            adj_list[u].push_back(make_pair(v, dist));
+            adj_list[v].push_back(make_pair(u, dist));
         }
 
         void print_graph(){
 
-            for (const auto& outerPair : adj_list) {
-                cout << "Node: " << outerPair.first << endl;
-
-                // Iterate through the inner map for each outer key
-                for (const auto& innerPair : outerPair.second) {
-                    cout << "    Neighbor: " << innerPair.first << " Edge Weight: " << innerPair.second << endl;
+            for (int i = 0; i < adj_list.size(); ++i) {
+                cout << "Node: " << i << endl;
+                for (const auto& edge : adj_list[i]) {
+                    cout << "    Neighbor: " << edge.first << " Edge Weight: " << edge.second << endl;
                 }
             }
 
         }
 
-        Graph(int n, double density, double dist_range){
+        Graph(int n, double density, int dist_range){
+
+            // Initialize adjacency list
+            vector<vector<pair<int, int> > > graph(n);
+            adj_list = graph;
 
             // Initialize a random number generator
             random_device rd;
-            mt19937 gen(rd());  // Mersenne Twister engine
+            mt19937 gen(rd());
 
             // Define Bernoulli and uniform distributions
             bernoulli_distribution bernoulliDist(density);
-            uniform_real_distribution<double> uniformDist(0.0, dist_range);
+            uniform_int_distribution<int> uniformDist(1, dist_range);
 
             // Randomly generate edges of the graph
-            for(int i = 1; i <= n; ++i){
-                for(int j = 1; j <= n; ++j){
+            for(int i = 0; i < n; ++i){
+                for(int j = 0; j < n; ++j){
                     if (i == j) continue;
                     bool result = bernoulliDist(gen);
                     if (result){
-                        double dist = uniformDist(gen);
-                        adj_list[i][j] = dist;
-                        adj_list[j][i] = dist;
+                        int dist = uniformDist(gen);
+                        this->add_edge(i, j, dist);
                     }
                 }
             }
@@ -61,7 +60,7 @@ class Graph {
 
 int main(){
 
-    Graph graph = Graph(10, 0.2, 1);
+    Graph graph = Graph(10, 0.2, 5);
     graph.print_graph();
     return 0;
 
